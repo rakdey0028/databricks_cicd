1a4e4915cfd9ef897ca1a601040a7aa0bedf6fc5
rakdey0028 <rakdey0028>
2025-04-12 16:55:34 +0000

    2nd commit

--- functions/cleaning_utils.py
+++ functions/cleaning_utils.py
@@ -1,22 +1,11 @@
-from pyspark.sql import DataFrame, functions as F
+#from pyspark.sql import DataFrame, functions as F
 
-def lowercase_all_column_names(df:DataFrame)->DataFrame:
-    """
-    Convert all column names to lower case. 
-    """
-    for col in df.columns:
-        df = df.withColumnRenamed(col, col.lower())
-    return df 
+from pyspark.sql.types import *
+import pyspark.sql.functions  as F
+def find_total(df):
 
-def uppercase_all_column_names(df:DataFrame)->DataFrame:
-    """
-    Convert all column names to upper case. 
-    """
-    for col in df.columns:
-        df = df.withColumnRenamed(col, col.upper())
-    return df 
-
-def add_metadata(df:DataFrame, field_dict:dict)->DataFrame:
-    for pair in field_dict.items():
-        df = df.withColumn(pair[0], F.lit(pair[1]))
-    return df
+  df = df.withColumn(
+      "Total",
+      F.aggregate("value", F.lit(0), lambda acc, x: acc + x)
+  )
+  return df.select('Total').collect()[0][0]
