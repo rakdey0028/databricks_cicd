86a705638ada479e6678a5bfa80b13f89c8f6008
rakdey0028 <rakdey0028>
2025-04-13 08:34:01 +0000

    hg

---
+++ functions/tests/main.py
@@ -0,0 +1,18 @@
+%pip install pytest
+import os
+import sys
+import pytest
+import test_cleaning_utils
+#import sum_func
+notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+repo_root = os.path.dirname(os.path.dirname(notebook_path))
+
+print(repo_root)
+
+dir=os.path.dirname(os.path.realpath('__file__'))
+print(dir)
+os.chdir(dir)
+sys.dont_write_bytecode = True
+print('---start---')
+pytest.main([".", "-p", "no:cacheprovider"])
+#pytest.main(["-v"])
