e90a197fd8ffecca6fdafdb64ae9c5606803bb66
rakdey0028 <rakdey0028>
2025-04-13 03:09:00 +0000

    ii

--- functions/tests/test_cleaning_utils.py
+++ functions/tests/test_cleaning_utils.py
@@ -3,23 +3,23 @@ from pyspark.sql import Row, SparkSession
 import pandas as pd
 from datetime import datetime
 import pytest
-#from databricks.connect import DatabricksSession
+from databricks.connect import DatabricksSession
 
 
 
 from functions.cleaning_utils import *
-@pytest.fixture(scope="session")
-def spark() -> SparkSession:
-  # Create a SparkSession (the entry point to Spark functionality) on
-  # the cluster in the remote Databricks workspace. Unit tests do not
-  # have access to this SparkSession by default.
-  return SparkSession.builder.getOrCreate()
+# @pytest.fixture(scope="session")
+# def spark() -> SparkSession:
+#   # Create a SparkSession (the entry point to Spark functionality) on
+#   # the cluster in the remote Databricks workspace. Unit tests do not
+#   # have access to this SparkSession by default.
+#   return SparkSession.builder.getOrCreate()
 
 
 from pyspark.sql.types import *
-@pytest.mark.usefixtures("spark")
+#@pytest.mark.usefixtures("spark")
 def test_findtotalfunc():
-  spark=SparkSession.builder.getOrCreate() #DatabricksSession.builder.getOrCreate() #
+  spark=DatabricksSession.builder.getOrCreate() #SparkSession.builder.getOrCreate() 
   cSchema = StructType([StructField("value", ArrayType(IntegerType()))])
   df=spark.createDataFrame(([[3,7,10]],),schema=cSchema)
   output_df = find_total(
