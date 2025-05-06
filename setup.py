0012fc75820a558b1f9fe0daa3073a71722a04f5
rakdey0028 <rakdey0028>
2025-05-06 20:03:10 +0000

    w

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="framework_wheel",
     version="0.1",
-    packages=find_packages(where="src"),
     package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
     install_requires=[
-        'pyspark==3.5.0',
+        'pyspark>=3.5.0',
     ],
 )
