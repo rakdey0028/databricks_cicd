3515ce59d678a6bb46574d2747b1b78a75e57561
rakdey0028 <rakdey0028>
2025-05-06 17:51:08 +0000

    ok

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="your_project_name",
     version="0.1",
-    packages=find_packages(where="src"),
+    packages=find_packages(where="./src"),
     package_dir={"": "src"},
     install_requires=[
         'pyspark==3.5.0',
